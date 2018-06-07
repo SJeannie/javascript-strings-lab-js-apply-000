3559312e540d3d3f38cf97091b5b03624ac69de8
Jeannie Kim <sj.jeannie.kim@gmail.com>
2018-06-07 02:47:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!"
 
 var topic = "space"
 
-var conversation = "Let's talk about "+`${topic}`+"."https://github.com/learn-co-students/javascript-strings-lab-js-apply-000/tree/solution
+var conversation = "Let's talk about "+`${topic}`+"."
